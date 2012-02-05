0bc38d314138052e16014bc37facca4d68479c9b
William Jennings <will.hunter.jennings@gmail.com>
2012-02-05 16:39:52 -0500

    edited the ofxCvBlob class and removed color control in draw & defined a seperate oFimage face to adjust and update with the alpha mix and face.crop, face.mirror and face.draw

--- week1/ScreenGrab/src/testApp.cpp
+++ week1/ScreenGrab/src/testApp.cpp
@@ -6,28 +6,34 @@ extern "C" {
 
 //--------------------------------------------------------------
 void testApp::setup(){
-
+    
+    
+    captureWidth = ofGetWidth();
+    captureHeight = ofGetHeight();
+    
 	finder.setup("haarcascade_frontalface_default.xml");
+    //finder.setup("parojos.xml"); 
 	//CGContextRef cgctx = NULL;
 	//ofSetVerticalSync(true);
-	//tex.allocate(300,300, GL_RGBA);
-	image.allocate(300, 300, OF_IMAGE_COLOR);
-	//pixels.allocate(300, 300, OF_IMAGE_COLOR);
+	//tex.allocate(captureWidth, captureHeight, GL_RGBA);
+	image.allocate(captureWidth, captureHeight, OF_IMAGE_COLOR);
+    face.allocate(captureWidth, captureHeight, OF_IMAGE_COLOR);
+	//pixels.allocate(captureWidth, captureHeight, OF_IMAGE_COLOR);
 	
 	ofSetFrameRate(30);
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
 	
-	int w = 300;
-	int h = 300;
-	
-	unsigned char * data = pixelsBelowWindow(ofGetWindowPositionX(),ofGetWindowPositionY(),w,h);
+    captureWidth = ofGetWidth();
+    captureHeight = ofGetHeight();
+    
+	data = pixelsBelowWindow(ofGetWindowPositionX(), ofGetWindowPositionY(), captureWidth, captureHeight);
 	
 	// now, let's get the R and B data swapped, so that it's all OK:
-	for (int i = 0; i < w*h; i++){
+	for (int i = 0; i < captureWidth * captureHeight; i++){
 		
 		unsigned char r = data[i*4]; // mem A  
 		
@@ -39,76 +45,92 @@ void testApp::update(){
 	
 	
 	if (data!= NULL) {
-		//tex.loadData(data, 300, 300, GL_RGBA);
+		//tex.loadData(data, captureWidth, captureHeight, GL_RGBA);
 		//tex.readToPixels(pixels);
 		//image = pixels;
-		image.setFromPixels(data, 300, 300, OF_IMAGE_COLOR_ALPHA, true);
+		image.setFromPixels(data, captureWidth, captureHeight, OF_IMAGE_COLOR_ALPHA, true);
 		image.setImageType(OF_IMAGE_COLOR);
 		image.update();
 		finder.findHaarObjects(image.getPixelsRef());
+        
+        face.setFromPixels (data, captureWidth, captureHeight,OF_IMAGE_COLOR_ALPHA,false); 
+        
+        face.setImageType(OF_IMAGE_COLOR);
+        
+        face.update();
 		
 	}
 	//cout << imageBelowWindow()[0] << endl;
 	
 	
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
 	image.draw(0,0, ofGetWidth(), ofGetHeight());
-	
+    
 	ofNoFill();
 	
 	//for each face "blob" we found, draw a rectangle around the face
     //#2
 	for(int i = 0; i < finder.blobs.size(); i++) {
-		ofRect(finder.blobs[i].boundingRect);
+		//ofRect(finder.blobs[i].boundingRect);
+        //finder.blobs[i].draw(); 
+        
+        //ofLine(finder.blobs[i].boundingRect.x, finder.blobs[i].boundingRect.y, finder.blobs[i].boundingRect.width, finder.blobs[i].boundingRect.height);
+        
+        // ofCircle(finder.blobs[i].boundingRect.x+ finder.blobs[i].boundingRect.width/2, finder.blobs[i].boundingRect.y + finder.blobs[i].boundingRect.height/2, 20);
+        
+        face.crop(finder.blobs[i].boundingRect.x, finder.blobs[i].boundingRect.y, finder.blobs[i].boundingRect.width, finder.blobs[i].boundingRect.height );
+        face.mirror(true,false);
+        face.draw(finder.blobs[i].boundingRect.x, finder.blobs[i].boundingRect.y, finder.blobs[i].boundingRect.width,finder.blobs[i].boundingRect.height);
 	}
+    
 	
 }
 
 //--------------------------------------------------------------
 void testApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::keyReleased(int key){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseMoved(int x, int y ){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseDragged(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mousePressed(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::mouseReleased(int x, int y, int button){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::windowResized(int w, int h){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::gotMessage(ofMessage msg){
-
+    
 }
 
 //--------------------------------------------------------------
 void testApp::dragEvent(ofDragInfo dragInfo){ 
-
+    
 }
--- week1/ScreenGrab/src/testApp.h
+++ week1/ScreenGrab/src/testApp.h
@@ -4,25 +4,31 @@
 #include "ofxOpenCv.h"
 
 class testApp : public ofBaseApp{
-
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
-		//ofTexture	tex;
-		//ofPixels	pixels;
-		ofImage		image;
-		ofxCvHaarFinder finder;	 // object used to detect haar features
-
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    
+    void keyPressed  (int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    int captureWidth;
+    int captureHeight;
+    
+    unsigned char * data;
+    
+    //ofTexture	tex;
+    //ofPixels	pixels;
+    ofImage		image;
+    ofImage     face;
+    ofxCvHaarFinder finder;	 // object used to detect haar features
+    
 };
